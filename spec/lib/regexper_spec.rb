f3318af53b5f4b5c3b7323b815fd4189b2ca590e
Jeff Avallone <jeff.avallone@gmail.com>
2012-12-26 00:57:16 -0500

    Adding failure tests for the parser

--- spec/lib/regexper_spec.rb
+++ spec/lib/regexper_spec.rb
@@ -8,6 +8,38 @@ describe Regexper do
       Regexper.parse('test').should be_a(Treetop::Runtime::SyntaxNode)
     end
 
+    context "invalid regular expressions" do
+
+      it "raises an exception for an unclosed character class" do
+        lambda { Regexper.parse('[abc') }.should raise_error
+      end
+
+      it "raises an exception for an unclosed sub-expression" do
+        lambda { Regexper.parse('(test') }.should raise_error
+      end
+
+      it "raises an exception for a '*' with nothing to repeat" do
+        lambda { Regexper.parse('*') }.should raise_error
+      end
+
+      it "raises an exception for a '+' with nothing to repeat" do
+        lambda { Regexper.parse('+') }.should raise_error
+      end
+
+      it "raises an exception for a '?' with nothing to repeat" do
+        lambda { Regexper.parse('?') }.should raise_error
+      end
+
+      it "raises an exception for '{}' syntax with nothing to repeat" do
+        lambda { Regexper.parse('{1,2}') }.should raise_error
+      end
+
+      it "raises an exception for empty '{}' syntax" do
+        lambda { Regexper.parse('x{}') }.should raise_error
+      end
+
+    end
+
   end
 
 end
