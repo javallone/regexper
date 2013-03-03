dd951945da95e4f8fe960d56a2b9319d1b6ed867
Julio Olivera <julio.olvr@gmail.com>
2013-03-03 20:34:57 -0300

    Avoid multiple renders by triggering "Display" quickly. Fixes #38

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,27 +10,34 @@ require.config({
     var form = document.getElementById('regexp_form'),
         input = document.getElementById('regexp_input'),
         error = document.getElementById('error'),
-        paper_container = document.getElementById('paper-container');
+        paper_container = document.getElementById('paper-container'),
+        current_request = null;
 
     form.onsubmit = function() {
+        if (current_request) {
+            return false;
+        }
+
         paper_container.innerHTML = '';
         error.innerHTML = '';
 
         document.body.className = 'is-loading';
 
-        new microAjax(form.action, function(text) {
+        current_request = new microAjax(form.action, function(text) {
             if (this.request.status == 200) {
                 document.body.className += ' has-results';
 
                 require(['regexper'], function(Regexper) {
                     Regexper.draw(paper_container, JSON.parse(text), function() {
                         document.body.className = 'has-results';
+                        current_request = null;
                         console.log("DONE");
                     });
                 });
             } else {
                 error.innerHTML = JSON.parse(text).error;
                 document.body.className = 'has-results';
+                current_request = null;
             }
         }, input.value);
 
